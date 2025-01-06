a0202b69e7584c108dab3a73e72374a59a4f196e
hieutdle <hieu.tdle@gmail.com>
2025-01-06 12:05:25 +0100

    add problem 70 and 704

---
+++ cpp/dynamic-programming/70.climbing-stairs.cpp
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode id=70 lang=cpp
+ *
+ * [70] Climbing Stairs
+ *
+ * https://leetcode.com/problems/climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (53.27%)
+ * Likes:    22633
+ * Dislikes: 906
+ * Total Accepted:    3.8M
+ * Total Submissions: 7.2M
+ * Testcase Example:  '2'
+ *
+ * You are climbing a staircase. It takes n steps to reach the top.
+ *
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can
+ * you climb to the top?
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: n = 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: n = 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= n <= 45
+ *
+ *
+ */
+
+#include <vector>
+
+using namespace std;
+// @lc code=start
+class Solution {
+ public:
+  int climbStairs(int n) {
+    if (n == 1) {
+      return 1;
+    }
+
+    vector<int> dp(n + 1, 0);
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i <= n; i++) {
+      dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n];
+  }
+};
+// @lc code=end
---
+++ py/dynamic-programming/70.climbing-stairs.py
@@ -0,0 +1,68 @@
+#
+# @lc app=leetcode id=70 lang=python3
+#
+# [70] Climbing Stairs
+#
+# https://leetcode.com/problems/climbing-stairs/description/
+#
+# algorithms
+# Easy (53.27%)
+# Likes:    22633
+# Dislikes: 906
+# Total Accepted:    3.8M
+# Total Submissions: 7.2M
+# Testcase Example:  '2'
+#
+# You are climbing a staircase. It takes n steps to reach the top.
+#
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can
+# you climb to the top?
+#
+#
+# Example 1:
+#
+#
+# Input: n = 2
+# Output: 2
+# Explanation: There are two ways to climb to the top.
+# 1. 1 step + 1 step
+# 2. 2 steps
+#
+#
+# Example 2:
+#
+#
+# Input: n = 3
+# Output: 3
+# Explanation: There are three ways to climb to the top.
+# 1. 1 step + 1 step + 1 step
+# 2. 1 step + 2 steps
+# 3. 2 steps + 1 step
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= n <= 45
+#
+#
+#
+
+# @lc code=start
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[n]
+
+
+# @lc code=end
