b7c1d2cd9909ccf07b6944753a43221c3773d004
Hieu Le <hieu.tdle@gmail.com>
2025-12-29 13:32:53 +0100

    add more guideline for 0424

--- src/sliding-window/0424_longest_repeating_character_replacement.h
+++ src/sliding-window/0424_longest_repeating_character_replacement.h
@@ -8,6 +8,21 @@ using std::unordered_map;
 
 class Solution {
  public:
+  // The difference between the two implementations is that
+
+  // In the first solution, once we find a larger window size
+  // we keep it and continue moving both the right and pointer
+  // The window is allowed to be invalid
+  // and the window size is only updated when a larger character frequency is found.
+  // We only care about the maxFreq + k = n - l (max window size we can get)
+
+  // In the second solution
+  // we continuously remove characters from the left until the window becomes valid again
+  // "ABABACB", k=2
+  // We expand the window until it reads "ABABAC": window size=6 maxFreq=3
+  // => "BABAC" window size= 5 maxFreq=3
+  // we advance the left pointer second time to get "ABAC".
+
   // Time: O(n) | Space: O(|Σ|) = O(26) = O(1)
   // Where Σ represents the character set (uppercase English letters)
   // Lazy Sliding Window
@@ -48,9 +63,6 @@ class Solution {
       ++count[s[r]];
       maxFreq = std::max(maxFreq, count[s[r]]);
 
-      // Condition: window size - maxFreq <= k
-      // Current window size: right - left + 1
-      // Characters to replace: window size - max frequency
       while (r - l + 1 - maxFreq > k) {
         --count[s[l]];
         ++l;
