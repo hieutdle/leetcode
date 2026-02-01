5b5dc1ac2f54b339ec4ef5671fdea5b32a99a7ec
Hieu Le <hieu.tdle@gmail.com>
2026-02-01 01:43:23 +0100

    improve problem 0057

--- src/intervals/0057_insert_interval.h
+++ src/intervals/0057_insert_interval.h
@@ -8,34 +8,26 @@ using std::vector;
 class Solution {
  public:
   // Time: O(n) | Space: O(1)
-  // Three phases: 
-  // 1. Add intervals before newInterval
-  // 2. Merge overlapping ones
-  // 3. Add the rest
   vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
     vector<vector<int>> res;
-    int n = intervals.size();
-    int i = 0;
 
-    // Add intervals that end before newInterval starts (no overlap)
-    while (i < n && intervals[i][1] < newInterval[0]) {
-      res.push_back(intervals[i]);
-      i++;
-    }
-
-    // Merge all intervals that overlap with newInterval
-    while (i < n && intervals[i][0] <= newInterval[1]) {
-      newInterval[0] = std::min(newInterval[0], intervals[i][0]);
-      newInterval[1] = std::max(newInterval[1], intervals[i][1]);
-      i++;
+    for (auto& interval : intervals) {
+      // Case 1: If new interval is before the current interval
+      // After adding new interval
+      // keep updating new interval to add the remaining intervals
+      if (newInterval[1] < interval[0]) {
+        res.push_back(newInterval);
+        newInterval = interval;
+      }  // Case 2: If new interval is after the current interval
+      else if (newInterval[0] > interval[1]) {
+        res.push_back(interval);
+      }  // Case 3: Overlapping interval
+      else {
+        newInterval[0] = std::min(newInterval[0], interval[0]);
+        newInterval[1] = std::max(newInterval[1], interval[1]);
+      }
     }
     res.push_back(newInterval);
-
-    // Add remaining intervals (start after merged interval)
-    while (i < n) {
-      res.push_back(intervals[i]);
-      i++;
-    }
 
     return res;
   }
